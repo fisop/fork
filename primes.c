a629f56ad9050d21c0b8c110d464aecfa2180ef3
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2023-08-24 21:09:04 -0300

    feat: add primes and xargs skels

---
+++ primes.c
@@ -0,0 +1,7 @@
+int
+main(int argc, char *argv[])
+{
+	// Your code here
+
+	return 0;
+}
---
+++ xargs.c
@@ -0,0 +1,11 @@
+#ifndef NARGS
+#define NARGS 4
+#endif
+
+int
+main(int argc, char *argv[])
+{
+	// Your code here
+
+	return 0;
+}
